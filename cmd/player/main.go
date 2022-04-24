987caf9560f3fe100d442079693700a02b150bd8
Yusuf Taiwo Hassan <teehazzan@gmail.com>
2022-04-24 11:56:21 +0100

    Modularizing the codebase

--- cmd/player/main.go
+++ cmd/player/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"time"
 
@@ -11,36 +10,82 @@ import (
 	"github.com/faiface/beep/speaker"
 )
 
+type Queue struct {
+	streamers []beep.Streamer
+}
+
+func (q *Queue) Add(streamers ...beep.Streamer) {
+	q.streamers = append(q.streamers, streamers...)
+}
+
+func (q *Queue) Stream(samples [][2]float64) (n int, ok bool) {
+	// We use the filled variable to track how many samples we've
+	// successfully filled already. We loop until all samples are filled.
+	filled := 0
+	for filled < len(samples) {
+		// There are no streamers in the queue, so we stream silence.
+		if len(q.streamers) == 0 {
+			for i := range samples[filled:] {
+				samples[i][0] = 0
+				samples[i][1] = 0
+			}
+			break
+		}
+
+		// We stream from the first streamer in the queue.
+		n, ok := q.streamers[0].Stream(samples[filled:])
+		// If it's drained, we pop it from the queue, thus continuing with
+		// the next streamer.
+		if !ok {
+			q.streamers = q.streamers[1:]
+		}
+		// We update the number of filled samples.
+		filled += n
+	}
+	return len(samples), true
+}
+
+func (q *Queue) Err() error {
+	return nil
+}
 
 func main() {
-	f, err := os.Open("static/3.mp3")
+	sr := beep.SampleRate(44100)
+	speaker.Init(sr, sr.N(time.Second/10))
+
+	// A zero Queue is an empty Queue.
+	var queue Queue
+	speaker.Play(&queue)
+	GetNextAudio(queue, sr)
+	select {}
+
+}
+
+func GetNextAudio(queue Queue, sr beep.SampleRate) {
+	var name string
+	fmt.Print("Type an MP3 file name: ")
+	fmt.Scanln(&name)
+
+	// Open the file on the disk.
+	f, err := os.Open(name)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		return
 	}
 
-	done := make(chan bool)
-
+	// Decode it.
 	streamer, format, err := mp3.Decode(f)
 	if err != nil {
-		log.Fatal(err)
-	}
-	defer streamer.Close()
-
-	speaker.Init(format.SampleRate*2, format.SampleRate.N(time.Second/10))
-
-	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
-		done <- true
-	})))
-
-	for {
-		select {
-		case <-done:
-			return
-		case <-time.After(time.Second):
-			speaker.Lock()
-			fmt.Println(format.SampleRate.D(streamer.Position()).Round(time.Second))
-			speaker.Unlock()
-		}
+		fmt.Println(err)
+		return
 	}
 
+	// The speaker's sample rate is fixed at 44100. Therefore, we need to
+	// resample the file in case it's in a different sample rate.
+	resampled := beep.Resample(4, format.SampleRate, sr, streamer)
+
+	// And finally, we add the song to the queue.
+	speaker.Lock()
+	queue.Add(resampled)
+	speaker.Unlock()
 }
