27aca7351a0b8ebf729aba2caf0d32dca1ed9c20
Yusuf Taiwo Hassan <teehazzan@gmail.com>
2022-04-25 09:09:07 +0100

    CleanUp Unused Echo code

---
+++ process/process.go
@@ -0,0 +1,87 @@
+package process
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
+	"github.com/teezzan/padio/internal/player"
+)
+
+var LoadingInProgress bool = false
+var StaticDir = "static"
+
+func init() {
+	var queue player.Queue
+	queue.Init()
+
+	sr := beep.SampleRate(44100)
+	speaker.Init(sr, sr.N(time.Second/10))
+
+	speaker.Play(beep.Seq(&queue))
+
+	for {
+
+		select {
+		case i := <-queue.Playing:
+			load := !i && !LoadingInProgress
+			if load {
+				LoadingInProgress = true
+				QueueAndPlay(&queue, sr)
+				LoadingInProgress = false
+			}
+		default:
+			fmt.Print("")
+		}
+	}
+}
+
+func QueueAndPlay(queue *player.Queue, sr beep.SampleRate) error {
+	f, err := os.Open(StaticDir)
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	files, err := f.Readdir(0)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	randomFile := files[rand.Intn(len(files))]
+	fmt.Println(randomFile.Name())
+	return PlayNextAudio(queue, sr, fmt.Sprintf("%s/%s", StaticDir, randomFile.Name()))
+}
+
+func PlayNextAudio(queue *player.Queue, sr beep.SampleRate, name string) error {
+
+	// Open the file on the disk.
+	f, err := os.Open(name)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	// Decode it.
+	streamer, format, err := mp3.Decode(f)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	// The speaker's sample rate is fixed at 44100. Therefore, we need to
+	// resample the file in case it's in a different sample rate.
+	resampled := beep.Resample(4, format.SampleRate, sr, streamer)
+
+	// And finally, we add the song to the queue.
+	speaker.Lock()
+	queue.Add(resampled)
+	speaker.Unlock()
+	fmt.Print("Added Successfully!")
+	return nil
+}
